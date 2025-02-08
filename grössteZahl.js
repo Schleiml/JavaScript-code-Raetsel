1790ff8ad6061b42e94d30ed8e4bb1094b198fa7
Schleiml <schleiml.ende@gmail.com>
2025-02-08 12:09:43 +0100

    geht das array durch un checkt was grösste zahl ist

---
+++ grössteZahl.js
@@ -0,0 +1,12 @@
+function gross(arr) {
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+console.log(gross([1, 3, 10, 4, 13, 3]));
